02da8a22e5432bcae479e4d6a4f05d5bb2f378a9
Mageba4life <andriesmageba107@gmail.com>
2024-10-29 12:15:57 +0200

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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" 
+const BOT_NUMBER = "27656319889"
+const SESSION_ID = "PRABATH-MD~47VBDajZ#5qdAEZTXqEqRt6-7G1K3KMXFdG63UqtgXVn5r6XyY6E"
 
 
 module.exports = {
